20a501620f25639efc3f74b4723b5fb5f6eee314
Aleksandr Zats <oleksandrzats@gmail.com>
2022-01-06 02:24:22 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Header />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Header,
+  },
+};
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: transparent;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+html {
+  background: url("./assets/Background.svg") left center;
+  background-size: cover;
+  background-color: #d9dbda;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="header">
+    <img class="header_logo" src="../assets/logo.png" alt="" />
+    <button class="header_burger"><div class="burger_line"></div></button>
+  </div>
+</template>
+
+<script></script>
+
+<style>
+.header {
+  background-color: #b3e6bc;
+  width: 100%;
+  padding: 14px 432px 14px 240px;
+  position: relative;
+  display: flex;
+  justify-content: flex-end;
+}
+.header_logo {
+  position: absolute;
+  top: -42px;
+  left: 240px;
+}
+.header_burger {
+  width: 41px;
+  height: 41px;
+  border-radius: 50%;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  background-color: transparent;
+  border-color: #0e2963;
+}
+
+.burger_line {
+  position: relative;
+  display: flex;
+  width: 23px;
+  height: 2px;
+  background-color: #0e2963;
+}
+.burger_line::before {
+  position: absolute;
+  top: -6px;
+  content: "";
+  width: 23px;
+  height: 2px;
+  background-color: #0e2963;
+}
+.burger_line::after {
+  position: absolute;
+  top: 6px;
+  content: "";
+  width: 23px;
+  height: 2px;
+  background-color: #0e2963;
+}
+
+.header_burger:hover {
+  background-color: #0e2963;
+  cursor: pointer;
+}
+
+.header_burger:hover .burger_line {
+  background-color: #fff;
+}
+.header_burger:hover .burger_line::before {
+  background-color: #fff;
+}
+.header_burger:hover .burger_line::after {
+  background-color: #fff;
+}
+</style>
